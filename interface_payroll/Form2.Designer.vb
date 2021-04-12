f49c836aae4ee1e38e83e75ecf35c4b0e61332fe
RogerNoel <noel.roger.pro@gmail.com>
2021-04-13 00:10:49 +0200

    Changement présentation liste des codes

--- interface_payroll/Form2.Designer.vb
+++ interface_payroll/Form2.Designer.vb
@@ -24,12 +24,14 @@ Partial Class form_codes
     Private Sub InitializeComponent()
         Me.lbl_code1 = New System.Windows.Forms.Label()
         Me.lbl_code1b = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'lbl_code1
         '
         Me.lbl_code1.AutoSize = True
-        Me.lbl_code1.Location = New System.Drawing.Point(13, 13)
+        Me.lbl_code1.Location = New System.Drawing.Point(8, 28)
         Me.lbl_code1.Name = "lbl_code1"
         Me.lbl_code1.Size = New System.Drawing.Size(37, 13)
         Me.lbl_code1.TabIndex = 0
@@ -38,26 +40,38 @@ Partial Class form_codes
         'lbl_code1b
         '
         Me.lbl_code1b.AutoSize = True
-        Me.lbl_code1b.Location = New System.Drawing.Point(57, 12)
+        Me.lbl_code1b.Location = New System.Drawing.Point(52, 27)
         Me.lbl_code1b.Name = "lbl_code1b"
         Me.lbl_code1b.Size = New System.Drawing.Size(14, 13)
         Me.lbl_code1b.TabIndex = 1
         Me.lbl_code1b.Text = "P"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lbl_code1b)
+        Me.GroupBox1.Controls.Add(Me.lbl_code1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(218, 235)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Codes de prestations assimilées"
         '
         'form_codes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.lbl_code1b)
-        Me.Controls.Add(Me.lbl_code1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "form_codes"
         Me.Text = "Liste des codes"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents lbl_code1 As Label
     Friend WithEvents lbl_code1b As Label
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
